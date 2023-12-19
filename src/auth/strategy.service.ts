2d5bc24c15d9315a0a85d60cf3ad54adeef68bd9
Marco DA SILVA <marco.dasilva@omnilog.fr>
2023-12-19 09:52:43 +0100

    fix prisma and docker

--- src/auth/strategy.service.ts
+++ src/auth/strategy.service.ts
@@ -27,7 +27,6 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(user: User) {
-    console.log(user);
     const { email } = user;
     const userData = await this.prismaService.user.findUnique({
       where: { email },
